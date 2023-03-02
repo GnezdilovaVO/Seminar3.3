6152ff34fa87de9e10a8aab9fe876bcde2bf46dd
Anton Vorontsov <antvork@gmail.com>
2023-03-02 16:07:56 +0700

    Изменил диаграмму и написал код

--- Example_002/Program.cs
+++ Example_002/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число ");
+int n =  Convert.ToInt32(Console.ReadLine());
+
+int i=0;
+while(n%10 != 0)
+{
+    n = n/10;
+    i++;
+}
+Console.WriteLine(i);
