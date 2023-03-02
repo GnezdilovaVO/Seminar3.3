97cd06b1900f238e2e89c3d7fb53349cea5309e4
Anton Vorontsov <antvork@gmail.com>
2023-03-02 16:16:40 +0700

    Оптимизировал Код Ex02

--- Example_002/Program.cs
+++ Example_002/Program.cs
@@ -1,10 +1,20 @@
-﻿Console.WriteLine("Введите число ");
-int n =  Convert.ToInt32(Console.ReadLine());
-
-int i=0;
-while(n%10 != 0)
+﻿int GetNumber(string text)
 {
-    n = n/10;
-    i++;
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
+
+int GetNumberOfDigits(int x)
+{
+    int res = 0;
+    while (x % 10 != 0)
+    {
+        x = x / 10;
+        res++;
+    }
+    return res;
+}
+
+int n = GetNumber("Введите число N: ");
+int i = GetNumberOfDigits(n);
 Console.WriteLine(i);
