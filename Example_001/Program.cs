8a06cc1396564e7fd1700fa08469847621e1225b
Anton Vorontsov <antvork@gmail.com>
2023-03-02 14:25:12 +0700

    end task22 & method

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetSum(int A)
+{
+    int b = 1;
+    int Sum = 0;
+    while (b <= A)
+    {
+        Sum = Sum + b;
+        b++;
+    }
+    return Sum;
+}
+
+int a = GetNumber("Введите число: ");
+int sum = GetSum(a);
+
+
+
+Console.WriteLine(sum);
